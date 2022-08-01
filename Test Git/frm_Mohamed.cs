1a6f95f54a7b87640c7ac984b21e020c364708d6
reaopop <58912886+reaopop@users.noreply.github.com>
2022-08-01 16:52:33 +0200

    Mohamed 's Update

---
+++ Test Git/frm_Mohamed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+
+namespace Test_Git
+{
+    public partial class frm_Mohamed : DevExpress.XtraEditors.XtraForm
+    {
+        public frm_Mohamed()
+        {
+            InitializeComponent();
+        }
+    }
+}
