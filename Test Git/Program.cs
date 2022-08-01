175aac5f6698ad6c45343655f6b932bcba37af8e
MinaMousa2022 <108571192+MinaMousa2022@users.noreply.github.com>
2022-08-01 17:24:13 +0200

    mina commennntnntntntnt

--- Test Git/Program.cs
+++ Test Git/Program.cs
@@ -18,6 +18,8 @@ namespace Test_Git
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             var x = "sssssssssssssssssssssssss";
+            ////////////////////////
+            ////////////////////////
         }
     }
 }
